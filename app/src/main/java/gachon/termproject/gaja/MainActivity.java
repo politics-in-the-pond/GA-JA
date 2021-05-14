a7d6c36418a1e7854f0f6b9fde53cb2735e69fba
Jeon Tak <tak980418@naver.com>
2021-05-14 13:52:22 +0900

    전탁 : 1. 하단 메뉴바 추가 2. 메인 화면 재구성 3. 게시글 작성 화면 추가

--- app/src/main/java/gachon/termproject/gaja/MainActivity.java
+++ app/src/main/java/gachon/termproject/gaja/MainActivity.java
@@ -1,23 +1,32 @@
 package gachon.termproject.gaja;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    FragmentManager manager;
-    FragmentTransaction ft;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-    BuyFragment fragmentBuy;
-    EatFragment fragmentEat;
-    TotalFragment fragmentTotal;
+import gachon.termproject.gaja.ui.alarm.AlarmFragment;
+import gachon.termproject.gaja.ui.home.HomeFragment;
+import gachon.termproject.gaja.ui.mypage.MypageFragment;
+
+public class MainActivity extends AppCompatActivity {
+   /* FragmentManager manager;
+    FragmentTransaction ft;*/
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private HomeFragment homeFragment = new HomeFragment();
+    private MypageFragment MyPageFragment = new MypageFragment();
+    private AlarmFragment AlarmFragment=new AlarmFragment();
 
     Button btn_goP;
 
@@ -26,18 +35,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        manager = getSupportFragmentManager();
+       // manager = getSupportFragmentManager();
 
-        fragmentBuy = new BuyFragment();
+       /* fragmentBuy = new BuyFragment();
         fragmentEat= new EatFragment();
-        fragmentTotal = new TotalFragment();
+        fragmentTotal = new TotalFragment();*/
 
-        ft = manager.beginTransaction();
-        ft.add(R.id.fragment_container, fragmentTotal).addToBackStack(null).commit();
+       // ft = manager.beginTransaction();
+
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.nav_host_fragment, homeFragment).commitAllowingStateLoss();
+        BottomNavigationView bottomNavigationView  = findViewById(R.id.nav_view);
+        bottomNavigationView .setOnNavigationItemSelectedListener(new ItemSelectedListener());
+
+        // ft.add(R.id.fragment_container, fragmentTotal).addToBackStack(null).commit();
 
     }
 
-    // spinner 이용해서 게시판 바꾸게 되면 fragment 이동
+   /* // spinner 이용해서 게시판 바꾸게 되면 fragment 이동
     public void onButtonClicked(View view) {
         ft = manager.beginTransaction();
 
@@ -54,6 +70,28 @@ public class MainActivity extends AppCompatActivity {
         else if((String.valueOf(spinner.getSelectedItem()).equalsIgnoreCase("같이 사요"))){
             ft.replace(R.id.fragment_container, fragmentBuy).commit();
            // Toast.makeText(this, String.valueOf(spinner.getSelectedItem()), Toast.LENGTH_LONG).show();
+        }
+    }*/
+
+    private class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.navigation_home:
+                    transaction.replace(R.id.nav_host_fragment, homeFragment).commitAllowingStateLoss();
+                    break;
+                case R.id.navigation_alarm:
+                    transaction.replace(R.id.nav_host_fragment,AlarmFragment).commitAllowingStateLoss();
+                    break;
+
+                case R.id.navigation_mypage:
+                    transaction.replace(R.id.nav_host_fragment, MyPageFragment).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
         }
     }
 }
---
+++ app/src/main/java/gachon/termproject/gaja/ui/writingPost/WritingPostActivity.java
@@ -0,0 +1,6 @@
+package gachon.termproject.gaja.ui.writingPost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WritingPostActivity extends AppCompatActivity {
+}
